54fa591bad5329f2385a09b054fca3cbb1276639
stevengroves187 <stevengroves187@gmail.com>
2021-05-31 18:31:39 -0500

    first commit

---
+++ src/theaters/theaters.service.js
@@ -0,0 +1,9 @@
+const knex = require("../db/connection");
+
+function list() {
+   return knex("theaters").join("movies_theaters", "movies_theaters.theater_id", "theaters.theater_id").join("movies", "movies_theaters.movie_id","movies.movie_id").select("*");
+}
+
+module.exports = {
+    list,
+};
